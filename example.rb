ff38868c804b6c8988006f4dd66083d5d85f17ea
Caius Durling <dev@caius.name>
2011-08-20 12:36:37 +0100

    Update example

--- example.rb
+++ example.rb
@@ -2,7 +2,7 @@
 require "rubygems"
 require "bundler/setup"
 
-require File.expand_path("lib/resque_picky_worker", File.dirname(__FILE__))
+require File.expand_path("lib/resque/picky_worker", File.dirname(__FILE__))
 
 Resque.redis = "localhost:6379"
 p Resque::PickyWorker.pick_queue

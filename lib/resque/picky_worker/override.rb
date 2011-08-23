0190d3c7ca6d4508e986f46c9a74e349f2d2a420
Caius Durling <dev@caius.name>
2011-08-23 22:16:36 +0100

    Silence warnings in override

--- lib/resque/picky_worker/override.rb
+++ lib/resque/picky_worker/override.rb
@@ -2,4 +2,7 @@ unless defined?(Resque::PickyWorker)
   require "resque/picky_worker"
 end
 
+# Swap this stuff out without generating a warning
+original_verbosity, $VERBOSE = $VERBOSE, nil
 Resque::Worker = Resque::PickyWorker
+$VERBOSE, original_verbosity = original_verbosity, nil
